7edb56ec137f45a840436b9abaf317dc4e266554
skuarch <skuarch@yahoo.com.mx>
2018-02-20 10:46:12 -0600

    wizeline

---
+++ src/components/todo/Main.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="container">
+    <br>
+    <h3>Todo App</h3>
+
+    <add-todo @onAddTodo="addTodo" />
+
+    <br>
+
+    <h4>Pending todos</h4>
+    <ul class="list-group">
+      <todo v-for="todo, index in pendingTodos"
+            :key="index"
+            :description="todo.description"
+            :done="todo.done"
+            @onToggle="togglePending(todo)"
+            @onRemove="remove(todo)"
+      />
+    </ul>
+
+    <br>
+
+    <h4>Completed todos</h4>
+    <ul class="list-group">
+      <todo v-for="todo, index in completedTodos"
+            :key="index"
+            :description="todo.description"
+            :done="todo.done"
+            @onToggle="toggleCompleted(todo)"
+            @onRemove="remove(todo)"
+      />
+    </ul>
+
+  </div>
+</template>
+
+<script>
+  import Todo from './Todo'
+  import AddTodo from './AddTodo'
+
+  export default {
+    data () {
+      return {
+        newTodo: '',
+
+        todos: [
+          { description: 'Grab a beer', done: true },
+          { description: 'Deliver the workshop', done: false },
+          { description: 'Learn VueJS', done: false }
+        ]
+      }
+    },
+
+    computed: {
+      pendingTodos () {
+        return this.todos.filter(todo => !todo.done)
+      },
+
+      completedTodos () {
+        return this.todos.filter(todo => todo.done)
+      }
+    },
+
+    methods: {
+      togglePending (todo) {
+        console.log('pending')
+        todo.done = !todo.done
+      },
+      toggleCompleted (todo) {
+        console.log('completed')
+        todo.done = !todo.done
+      },
+
+      addTodo (newTodo) {
+        this.todos.push({
+          description: newTodo,
+          done: false
+        })
+      },
+
+      remove (todo) {
+        this.todos.splice(this.todos.indexOf(todo), 1)
+      }
+    },
+
+    components: {
+      Todo,
+      AddTodo
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/todo/Todo.vue
@@ -0,0 +1,35 @@
+<template>
+  <li class="list-group-item">
+    <button
+      @click="remove"
+      class="btn btn-sm btn-danger"
+    >x
+    </button>
+    {{ description }} - {{ done ? 'done' : 'nope' }}
+    <button
+      @click="toggle"
+      class="btn btn-sm btn-primary float-right">
+      toggle!
+    </button>
+  </li>
+</template>
+
+<script>
+  export default {
+    props: ['description', 'done'],
+
+    methods: {
+      toggle () {
+        this.$emit('onToggle')
+      },
+
+      remove () {
+        this.$emit('onRemove')
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
